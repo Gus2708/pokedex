0a4f105f22b8a697ae6bdbb4439476a5c283697e
Gustavo <gustavoalfonso27@gmail.com>
2021-12-19 00:22:50 -0400

    adding styles

--- src/App.js
+++ src/App.js
@@ -34,14 +34,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <form onSubmit={getSearch}>
-        <input type="text" placeholder="Search for Pokemon..." onChange={updateSearch} value={search}/>
-        <button type="submit">Search</button>
+    <div className="header">
+      <h1>Pokedex</h1>
+      <h2>(ポケモン</h2>
+    </div>
+      <form onSubmit={getSearch} className="search">
+        <input type="text" placeholder="Search for Pokemon..." onChange={updateSearch} value={search} className="input"/>
+        <button type="submit" className="btn">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+        </button>
       </form>
-      {pokemons.map(pokemons => (
-        <Card name={pokemons.name} url={pokemons.url} key={pokemons.name + Date.now()}/>
-      ))};
+      <div className="pokeContainer">
+        {pokemons.map(pokemons => (
+          <Card name={pokemons.name} url={pokemons.url} key={pokemons.name + Date.now()}/>
+        ))};
+      </div>
     </div>
   );
 }
