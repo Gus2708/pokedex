42d6e36060a7081f3709167fb463d00fd00d0c10
Gustavo <gustavoalfonso27@gmail.com>
2021-12-20 17:15:47 -0400

    Card  style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const [search, setSearch] = useState('');
   const [pokemons, setPokemons] = useState([]);
-  const [query, setQuery] = useState(`pokemon?limit=100&offset=200`)
+  const [query, setQuery] = useState(`pokemon?limit=100&offset=0`)
   
   useEffect(() => {
     getPokemons();
@@ -29,7 +29,7 @@ function App() {
 
   const getSearch = e => {
     e.preventDefault();
-    setQuery(`pokemon/${search}`);
+    
   }
 
   return (
@@ -41,7 +41,7 @@ function App() {
       <form onSubmit={getSearch} className="search">
         <input type="text" placeholder="Search for Pokemon..." onChange={updateSearch} value={search} className="input"/>
         <button type="submit" className="btn">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
         </button>
       </form>
       <div className="pokeContainer">
--- src/Card.js
+++ src/Card.js
@@ -18,10 +18,13 @@ const Card = ({name, url}) => {
     }
     
     return(
-        <div>
-            <svg className="pokeball" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#ecebf3c4"><rect fill="none" height="24" width="24"/><path d="M14.5,12c0,1.38-1.12,2.5-2.5,2.5c-1.38,0-2.5-1.12-2.5-2.5s1.12-2.5,2.5-2.5C13.38,9.5,14.5,10.62,14.5,12z M22,12 c0,5.52-4.48,10-10,10C6.48,22,2,17.52,2,12S6.48,2,12,2C17.52,2,22,6.48,22,12z M20,12h-4c0-2.21-1.79-4-4-4c-2.21,0-4,1.79-4,4H4 c0,4.41,3.59,8,8,8C16.41,20,20,16.41,20,12z"/></svg>
-            <img src={image} alt="Pokemon img" />
-            <h1>{name}</h1>
+        <div className="container">
+            <svg xmlns="http://www.w3.org/2000/svg" className="pokeballBack"  version="1.1" id="mdi-pokeball" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4C7.92,4 4.55,7.05 4.06,11H8.13C8.57,9.27 10.14,8 12,8C13.86,8 15.43,9.27 15.87,11H19.94C19.45,7.05 16.08,4 12,4M12,20C16.08,20 19.45,16.95 19.94,13H15.87C15.43,14.73 13.86,16 12,16C10.14,16 8.57,14.73 8.13,13H4.06C4.55,16.95 7.92,20 12,20M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10Z" /></svg>
+            <div className="card">
+                <svg className="pokeball" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"><rect fill="none" height="24" width="24"/><path d="M14.5,12c0,1.38-1.12,2.5-2.5,2.5c-1.38,0-2.5-1.12-2.5-2.5s1.12-2.5,2.5-2.5C13.38,9.5,14.5,10.62,14.5,12z M22,12 c0,5.52-4.48,10-10,10C6.48,22,2,17.52,2,12S6.48,2,12,2C17.52,2,22,6.48,22,12z M20,12h-4c0-2.21-1.79-4-4-4c-2.21,0-4,1.79-4,4H4 c0,4.41,3.59,8,8,8C16.41,20,20,16.41,20,12z"/></svg>
+                <img src={image} alt="Pokemon img" />
+                <h1>{name}</h1>
+            </div>
         </div>
     );
 }
